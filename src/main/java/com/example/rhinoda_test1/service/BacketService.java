27438defcede68fb038d978e0e4a0b2a89fb7f4d
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 14:15:22 +0400

    Service_6

--- src/main/java/com/example/rhinoda_test1/service/BacketService.java
+++ src/main/java/com/example/rhinoda_test1/service/BacketService.java
@@ -1,9 +1,9 @@
 package com.example.rhinoda_test1.service;
 
-import com.example.rhinoda_test1.controller.BacketController;
 import com.example.rhinoda_test1.entity.Backet;
 import com.example.rhinoda_test1.entity.CalculatedBacket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.net.http.HttpClient;
@@ -14,20 +14,25 @@ public class BacketService {
     @Autowired
     HttpClient httpClient;//изображает другой микросервис
 
-    public CalculatedBacket CalculateBacketCost(Backet backet){//вернуть стоимость, используя внешний микросервис
+    public CalculatedBacket CalculateBacketCost(Backet backet) {//вернуть стоимость, используя внешний микросервис
 //получить полную стоимость, собрать через конструктор расчитаную корзину и вернуть
 
         var sumOfPosition = 0;// получить из кэша, если их там нет, берём из микросервиса и кладём в кэш
         //использовать springframework.cache
-        var totalSum = httpClient.send(backet.getAmountProducts()+ sumOfPosition, responseHandler());//взять количество товаров и сумму по позиции, кинуть в другой микросервис по http и получить из него ответ
+        var totalSum = httpClient.send(backet.getAmountProducts() + sumOfPosition, responseHandler());//взять количество товаров и сумму по позиции, кинуть в другой микросервис по http и получить из него ответ
 
-        var calculatedBacket = new CalculatedBacket(backet.getProductId(),backet.getAmountProducts(),sumOfPosition,totalSum);
+        var calculatedBacket = new CalculatedBacket(backet.getProductId(), backet.getAmountProducts(), sumOfPosition, totalSum);
 
         return calculatedBacket;
     }
 
     //сделать метод для получения суммы по позиции из кэша
-    private float getSumOfPosition(){
-
+    @Cacheable(cacheNames = "sumOfPosition")
+    private float getSumOfPosition(Backet backet) {
+        var sumOfPosition = 0;
+        if (sumOfPosition == 0) {//если ничего нет, идём в другой микросервис, берём данные оттуда и помещаем в кэш
+            sumOfPosition = httpClient.send(backet.getProductId(), requestHandler());//получим данные из микросервиса по productId
+        }
+        return sumOfPosition;
     }
 }
