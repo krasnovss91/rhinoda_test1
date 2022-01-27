6acef0c50b479d22cc82495be03cb9d88910f31c
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 12:25:15 +0400

    Service_3

--- src/main/java/com/example/rhinoda_test1/service/BacketService.java
+++ src/main/java/com/example/rhinoda_test1/service/BacketService.java
@@ -6,17 +6,19 @@ import com.example.rhinoda_test1.entity.CalculatedBacket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.http.HttpClient;
+
 @Service
 public class BacketService {
 
     @Autowired
-    BacketController backetController;
+    HttpClient httpClient;//изображает другой микросервис
 
     public CalculatedBacket CalculateBacketCost(Backet backet){//вернуть стоимость, используя внешний микросервис
 //получить полную стоимость, собрать через конструктор расчитаную корзину и вернуть
 
         var sumOfPosition = 0;// получить из кэша
-        var totalSum = 0;//получить из другого микросервиса по http
+        var totalSum = 0;//взять количество товаров и сумму по позиции, кинуть в другой микросервис по http и получить из него ответ
 
     }
 }
