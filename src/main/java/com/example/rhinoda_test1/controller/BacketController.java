9d7bacd069a202a359d6bdc7f83d7ee613545d60
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 11:59:41 +0400

    Controller_1

--- src/main/java/com/example/rhinoda_test1/controller/BacketController.java
+++ src/main/java/com/example/rhinoda_test1/controller/BacketController.java
@@ -1,4 +1,25 @@
 package com.example.rhinoda_test1.controller;
 
+import com.example.rhinoda_test1.entity.Backet;
+import com.example.rhinoda_test1.entity.CalculatedBacket;
+import com.example.rhinoda_test1.service.BacketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BacketController {
+
+    @Autowired
+    BacketService backetService;
+
+    @PostMapping("/getBacket")
+    public void getBacket(Backet backet){//поймать по сети объект "Корзина" и передать его сервису
+
+    }
+
+    public CalculatedBacket setCalculatedBacket(){//
+
+    }
+
 }
