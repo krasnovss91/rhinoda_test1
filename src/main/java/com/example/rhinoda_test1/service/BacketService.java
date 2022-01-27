6bfde853c61c0fb292e714e741556181f43ab275
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 11:45:52 +0400

    Service_1

--- src/main/java/com/example/rhinoda_test1/service/BacketService.java
+++ src/main/java/com/example/rhinoda_test1/service/BacketService.java
@@ -1,4 +1,17 @@
 package com.example.rhinoda_test1.service;
 
+import com.example.rhinoda_test1.controller.BacketController;
+import com.example.rhinoda_test1.entity.Backet;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BacketService {
+
+    @Autowired
+    BacketController backetController;
+
+    public float CalculateTotalSum(Backet backet){//вернуть стоимость, используя внешний микросервис
+
+        var totalSum = 0;
+        return totalSum;
+    }
 }
