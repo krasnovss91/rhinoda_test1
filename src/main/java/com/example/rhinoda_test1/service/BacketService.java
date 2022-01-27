4e6a5cfdb28dcd98158fdcde5ae81f6a450b264d
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 11:50:37 +0400

    Service_2

--- src/main/java/com/example/rhinoda_test1/service/BacketService.java
+++ src/main/java/com/example/rhinoda_test1/service/BacketService.java
@@ -3,7 +3,9 @@ package com.example.rhinoda_test1.service;
 import com.example.rhinoda_test1.controller.BacketController;
 import com.example.rhinoda_test1.entity.Backet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BacketService {
 
     @Autowired
