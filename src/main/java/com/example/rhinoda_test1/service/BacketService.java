fc663a5f24db0257ef21c9d7666adad359501646
krasnovss91 <krasnovstanislav91@gmail.com>
2022-01-27 14:26:41 +0400

    Service_7

--- src/main/java/com/example/rhinoda_test1/service/BacketService.java
+++ src/main/java/com/example/rhinoda_test1/service/BacketService.java
@@ -17,11 +17,11 @@ public class BacketService {
     public CalculatedBacket CalculateBacketCost(Backet backet) {//вернуть стоимость, используя внешний микросервис
 //получить полную стоимость, собрать через конструктор расчитаную корзину и вернуть
 
-        var sumOfPosition = 0;// получить из кэша, если их там нет, берём из микросервиса и кладём в кэш
+        var sumOfPosition = getSumOfPosition(backet);
         //использовать springframework.cache
         var totalSum = httpClient.send(backet.getAmountProducts() + sumOfPosition, responseHandler());//взять количество товаров и сумму по позиции, кинуть в другой микросервис по http и получить из него ответ
 
-        var calculatedBacket = new CalculatedBacket(backet.getProductId(), backet.getAmountProducts(), sumOfPosition, totalSum);
+        var calculatedBacket = new CalculatedBacket(backet.getProductId(), backet.getAmountProducts(), sumOfPosition, totalSum);//соберём через конструктор объект расчитанной корзины
 
         return calculatedBacket;
     }
@@ -29,7 +29,7 @@ public class BacketService {
     //сделать метод для получения суммы по позиции из кэша
     @Cacheable(cacheNames = "sumOfPosition")
     private float getSumOfPosition(Backet backet) {
-        var sumOfPosition = 0;
+        var sumOfPosition = 0;//инициализировали переменную для данных из кэша
         if (sumOfPosition == 0) {//если ничего нет, идём в другой микросервис, берём данные оттуда и помещаем в кэш
             sumOfPosition = httpClient.send(backet.getProductId(), requestHandler());//получим данные из микросервиса по productId
         }
